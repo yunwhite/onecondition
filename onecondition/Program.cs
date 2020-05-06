19d19ee6df3f2e51a58ea8dcd62257e3a6027238
yunwhite <s2018s12@e-mirim.hs.kr>
2020-05-06 23:02:28 +0900

    04 문제

--- onecondition/Program.cs
+++ onecondition/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,11 @@ namespace onecondition
     {
         static void Main(string[] args)
         {
+            int x = 15;
+            if (x > 10 && x < 20)
+            {
+                Console.WriteLine("조건에 맞습니다.");
+            }
         }
     }
 }
